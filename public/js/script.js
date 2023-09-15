5ecb15df0a4d6d048a15480aca24b84f3d12ce52
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2023-09-15 17:47:07 -0300

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,41 @@
+const nameInput = document.getElementById('name-input');
+const playButton = document.getElementById('play-button');
+const numbers = document.getElementById('numbers');
+const guessInput = document.getElementById('guess-input');
+const guessButton = document.getElementById('guess-button');
+
+const formName = document.getElementById('form-name');
+const game = document.getElementById('game');
+
+/** @type {HTMLTableElement} */
+const table = document.getElementById('table');
+
+function play() {
+  const name = nameInput.value;
+
+  const socket = io({ query: { name } });
+
+  formName.classList.add('hidde');
+  game.classList.remove('hidde');
+
+  socket.on('update-clients', (data) => {
+    let rows = '';
+    data.forEach(
+      ({ name, score }) =>
+        (rows += `<tr><td>${name}</td><td>${score}</td></tr>`)
+    );
+    table.tBodies[0].innerHTML = rows;
+  });
+
+  socket.on('numbers', (data) => {
+    numbers.innerText = data.length > 0 ? data : '...';
+  });
+
+  function sendGuess() {
+    socket.emit('guess', guessInput.value);
+  }
+
+  guessButton.addEventListener('click', sendGuess);
+}
+
+playButton.addEventListener('click', play);
---
+++ socket.js
@@ -0,0 +1,52 @@
+import { Server } from 'socket.io';
+import server from './app.js';
+
+const io = new Server(server);
+
+let clients = [];
+let numbers = [];
+let secretNumber = 10;
+
+io.on('connection', (socket) => {
+  const name = socket.handshake.query.name;
+  clients.push({ name, id: socket.id, score: 0 });
+  io.emit('update-clients', clients);
+
+  socket.emit(
+    'numbers',
+    numbers.sort((a, b) => a - b)
+  );
+
+  socket.on('guess', (data) => {
+    if (!Number.isNaN(data)) {
+      if (data >= 1 && data <= 100 && !numbers.includes(data)) {
+        numbers.push(data);
+
+        if (secretNumber == data) {
+          const client = clients.find((e) => e.id == socket.id);
+          client.score++;
+          clients.sort((a, b) => b.score - a.score);
+
+          io.emit('update-clients', clients);
+
+          secretNumber = 1 + Math.floor(Math.random() * 100 - 1);
+          numbers = [];
+        }
+
+        io.emit(
+          'numbers',
+          numbers.sort((a, b) => a - b)
+        );
+      }
+    }
+  });
+
+  socket.on('disconnect', (e) => {
+    clients = clients.filter((e) => e.id != socket.id);
+    io.emit('update-clients', clients);
+  });
+});
+
+server.listen(3000);
+
+export default io;
